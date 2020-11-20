a6e9d84afd2968c190821a6243a70615055e4782
Eliza Vardanyan <evarda17@gmail.com>
2020-11-19 23:38:50 -0500

    Finishing up with saving the caption with the image yet to dismiss the screen and return to another page

--- Instagram_parse/CameraViewController.swift
+++ Instagram_parse/CameraViewController.swift
@@ -27,6 +27,24 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     @IBAction func onSubmitButton(_ sender: Any) {
         
+        let post = PFObject(className: "Pets")
+        
+        post["caption"] = commentField.text!
+        post["author"] =  PFUser.current()!
+        
+        let imageData =  imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] =  file
+        
+        post.saveInBackground { (success, error) in
+            if success{
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else{
+                print("Error! Could not save it")
+            }
+        }
         
         
     }
