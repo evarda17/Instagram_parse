0372b3a749f7b2ec16bb139f73ae42b1e5c9b8da
Eliza Vardanyan <evarda17@gmail.com>
2020-11-20 20:30:53 -0500

    should be able to upload the image ( does not work yet)

---
+++ Instagram_parse/PostCell.swift
@@ -0,0 +1,32 @@
+//
+//  PostCell.swift
+//  Instagram_parse
+//
+//  Created by Eliza Vardanyan on 11/20/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
